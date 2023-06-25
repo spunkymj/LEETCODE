cdae1287b649a5d882583f7ad0f3c6582038e7fd
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-06-25 14:54:44 +0530

    Time: 250 ms (32.05%), Space: 12 MB (27.62%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int mod=1e9+7;
-
-    int countRoutes(vector<int>& l, int start, int finish, int fuel) {
-        int n=l.size();
-        vector<vector<int>> dp(n,vector<int>(fuel+1,0));
-        dp[start][fuel]=1;
-        for(int f=fuel ; f>=0 ; f--){
-            for(int i=0 ; i<n ; i++){
-                if(!dp[i][f] || abs(l[i]-l[finish])>f){
-                    continue;
-                }
-                for(int j=0 ; j<n ; j++){
-                    int d=abs(l[j]-l[i]);
-                    if(i==j || (d>f)){
-                        continue;
-                    }
-                    
-                    dp[j][f-d]+=dp[i][f];
-                    dp[j][f-d]%=mod;
-                }
+    
+    int solve(vector<int>& locations, int start, int finish, int fuel,vector<vector<int>> &dp){
+        if(dp[start][fuel]!=-1){
+            return dp[start][fuel];
+        }
+        
+        int ans=0;
+        if(start==finish){
+            ans++;
+        }
+        
+        for(int i=0 ; i<locations.size() ; i++){
+            if(i==start){
+                continue;
+            }
+            int reduce=abs(locations[i]-locations[start]);
+            if(reduce<=fuel){
+                ans+=solve(locations,i,finish,fuel-reduce,dp);
+                ans=ans%mod;
             }
         }
 
-        int ans=0;
-        for(int f=0 ; f<=fuel ; f++){
-            ans+=dp[finish][f];
-            ans=ans%mod;
-        }
+        return dp[start][fuel]=ans;
+    }
 
-        return ans;
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        vector<vector<int>> dp(locations.size(),vector<int>(fuel+1,-1));
+        return solve(locations,start,finish,fuel,dp);
     }
 };
