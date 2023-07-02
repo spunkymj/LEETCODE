6c1b766b2b3a7894ec8436b45f511e48db196c6b
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-07-02 13:01:32 +0530

    Time: 362 ms (38.46%), Space: 150 MB (7.69%) - LeetHub

---
+++ 6911-continuous-subarrays/6911-continuous-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        multiset<int> s;
+        long long ans=0;
+        for (int i=0, j=0; i<nums.size(); ++i) {
+            s.insert(nums[i]);
+            while(*s.rbegin()-*s.begin()>2) {
+                s.erase(s.find(nums[j++]));
+            }
+            ans+=i-j+1;
+        }
+        return ans;
+    }
+};
