2f5ee8d19995e329609f811fb188797a4ee2cd82
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-07-04 23:44:13 +0530

    Time: 14 ms (20.77%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //   3 is written in binary as 1 1 (x1 x0)
-        int x0=0;
-        int x1=0;
-        for(int &ele : nums){
-            x1=x1^(x0&ele);
-            x0=x0^ele;
-            int mask=~(x0&x1); 
-            x1=x1&mask;
-            x0=x0&mask;
+        vector<int> biti(32,0);
+        for(int i=0 ; i<nums.size() ; i++){
+            for(int j=0 ; j<32 ; j++){
+                if(nums[i]&(1<<j)){
+                    biti[j]++;
+                }
+            }
         }
-        return x0;
+        
+        long long ans=0;
+        for(int i=0 ; i<32 ; i++){
+            if(biti[i]%3==0){
+                continue;
+            }
+            else{
+                ans+=(1<<i);
+            }
+        }
+        return ans;
     }
 };
