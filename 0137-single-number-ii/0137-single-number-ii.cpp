340664e48547ba378cb73c0121881369b8e2a33f
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-07-04 17:16:58 +0530

    Time: 12 ms (35.14%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> bitpos(32,0),bitneg(32,0);
-        for(int i=0 ; i<nums.size() ; i++){
-            if(nums[i]>=0){
-                for(int j=0 ; j<32 ; j++){
-                    if(nums[i]&(1<<j)){
-                        bitpos[j]++;
-                    }
-                }
-            }
-            else{
-                for(int j=0 ; j<32 ; j++){
-                    if(nums[i]&(1<<j)){
-                        bitneg[j]++;
-                    }
-                }
-            }
+        //   3 is written in binary as 1 1 (x1 x0)
+        int x0=0;
+        int x1=0;
+        for(int &ele : nums){
+            x1=x1^(x0&ele);
+            x0=x0^ele;
+            int mask=~(x0&x1); 
+            x1=x1&mask;
+            x0=x0&mask;
         }
-        
-        long long ans=0;
-        bool neg=false;
-        for(int i=0 ; i<32 ; i++){
-            if(bitpos[i]%3==0 && bitneg[i]%3==0){
-                continue;
-            }
-            else{
-                if(bitneg[i]%3!=0){
-                    neg=true;
-                    ans+=(1<<i);
-                }
-                else{
-                    ans+=(1<<i);
-                }
-            }
-        }
-        return ans;
+        return x0;
     }
 };
