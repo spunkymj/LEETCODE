cbd39bd50204998900a868882f95b21329ea0632
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-07-09 00:26:42 +0530

    Time: 32 ms (62.50%), Space: 65.2 MB (75.00%) - LeetHub

---
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int alternatingSubarray(vector<int>& nums) {
+        int ans=0;
+        vector<int> val={1,-1};
+        for(int i=0 ; i<nums.size()-1 ; i++){
+            int j=i+1;
+            int id=0;
+            if(nums[j]-nums[i]==1){
+                while(j<nums.size() && (nums[j]-nums[j-1]==val[id])){
+                    j++;
+                    id=!id;
+                }
+                ans=max(ans,j-i);
+            }
+        }
+        return ans==0?-1 : ans;
+    }
+};
