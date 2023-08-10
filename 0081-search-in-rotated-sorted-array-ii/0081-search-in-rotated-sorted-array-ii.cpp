64c062d6583f900acef88e85400fa450d3f8adbb
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-08-10 22:38:13 +0530

    Time: 3 ms (91.34%), Space: 14 MB (50.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,65 +1,36 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n=nums.size();  
-        int l=0;
-        int r=n-1;
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] == target)
+                return true;
 
-        //pivot
-        if ((nums[l]==nums[r]) && (nums[l]==target)){
-            return 1;
-        }
-        
-        while((l+1<n)&&(nums[l+1]==nums[0])){
-            l++;
-        }
-        
-        while ((r>=0)&&(nums[r]==nums[0])){
-            r--;
-        }
-        
-        int start=l;
-        int end=r;
-        
-        int pivot=-1;
-        while(l<=r){
-            int mid=l+((r-l)/2);
-            if (nums[mid]>=nums[0]){
-                l=mid+1;
+			// update left & right due to duplicates
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
             }
-            else{
-                pivot=mid;
-                r=mid-1;
+            else if(nums[l] <= nums[mid])
+            {
+                if((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            else
+            {
+                if((nums[mid] < target) && (nums[r]>= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
-
-        
-        if (pivot==-1){
-            l=start;
-            r=end;
-        }
-        else if ((target>=nums[pivot]) && ( target<=nums[end])){
-            l=pivot;
-            r=end;
-        }
-        else{
-            l=start;
-            r=pivot-1;
-        }
-        
-        //Binary Search
-        while(l<=r){
-            int mid=l+((r-l)/2);
-            if (nums[mid]==target){
-                return 1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
-        }
-    return 0;
+        return false;
     }
 };
