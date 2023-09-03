bf1e60722fa13c64d69022f69248823e7ea5404b
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-09-03 10:22:48 +0530

    Time: 164 ms (70.00%), Space: 112.5 MB (70.00%) - LeetHub

---
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int mod, int k) {        
+        unordered_map<long long,long long> mp;
+        long long ans = 0;
+        int prefix = 0;
+        int n = nums.size();
+        mp[0]++;
+        // We will take Prefix Sum of nums[i]%mod=k
+        // For Example [3,1,9,6] [1,0,1,1] 
+        // prefix = [1,1,2,2] if its count==k till index i
+
+        for(int i=0;i<n;i++) {
+            // if my current element%m=k
+            if(nums[i]%mod==k){
+                prefix++;
+            }
+            prefix%=mod;
+            if(mp.find((prefix-k+mod)%mod)!=mp.end()){
+                ans+=mp[(prefix-k+mod)%mod];
+            }
+            mp[prefix]++;
+        }
+        return ans;
+    }
+};
+
