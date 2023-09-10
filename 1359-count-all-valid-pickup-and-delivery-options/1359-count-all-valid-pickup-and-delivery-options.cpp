245d3de3582f01922706f3fe090c6cf2cf5f49e4
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-09-10 17:37:17 +0530

    Time: 5 ms (18.65%), Space: 6 MB (71.30%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,57 @@
+#define ll long long
+
+ll N=1002;
+ll mod=1e9+7;
+
+vector<ll> fac(N, 1);
+vector<ll> invfac(N,1);
+
+ll binpow(ll x, ll y)
+{
+    ll ans = 1;
+    while (y > 0)
+    {
+        if (y % (ll)2 == 1)
+        {
+            ans *= x;
+            ans %= mod;
+        }
+        x = (x * x) % mod;
+        y /= (ll)2;
+    }
+    return ans;
+}
+
+ll ncr(ll n, ll r)
+{
+    ll nf = fac[n];
+    ll rf = invfac[r];
+    ll nmrf = invfac[n - r];
+    ll ret = nf;
+    ret *= rf;
+    ret %= mod;
+    ret *= nmrf;
+    ret %= mod;
+    return ret;
+}
+
+void processFact()
+{
+    for (ll i = 1; i < N; i++)
+        fac[i] = (fac[i - 1] * i) % mod;
+    for (ll i = 0; i < N; i++)
+        invfac[i] = binpow(fac[i], mod - 2);
+}
+
+class Solution {
+public:
+    int countOrders(int n) {
+        processFact();
+        ll ans=fac[2*n];
+        for(int i=0 ; i<n ; i++){
+            ans*=invfac[2];
+            ans%=mod;
+        }
+        return ans%mod;
+    }
+};
