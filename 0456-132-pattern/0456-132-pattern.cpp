b8d291a022d2300c0630bb6986042604717af807
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-09-30 19:36:02 +0530

    Time: 78 ms (19.01%), Space: 48.9 MB (37.36%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int maxi = INT_MIN;
+        stack<int> st;
+        for( int i = nums.size()-1; i >= 0; i -- ){
+            if( nums[i] < maxi ) {
+                return true;
+            }
+            else{
+                while( !st.empty() && nums[i] > st.top() ){ 
+                    maxi = st.top(); 
+                    st.pop(); 
+                }
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
