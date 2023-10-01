ddf8b62f04913971347b8dacad2d58f8be3c4945
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-10-01 10:23:28 +0530

    Time: 17 ms (46.37%), Space: 10 MB (55.04%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j=0;
+        
+        for(int i=0;i<=s.size();i++){
+            if(s[i]==' '|| s[i]=='\0')
+            {
+                reverse(s.begin()+j,s.begin()+i);
+                j=i+1;
+            }
+        }
+        return s;
+    }
+};
