013018571b69cf0f828e3de1bd26c63502d3120e
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-10-08 09:57:50 +0530

    Time: 4 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

---
+++ 2896-apply-operations-to-make-two-strings-equal/2896-apply-operations-to-make-two-strings-equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(string s1, string s2, int x) {
+        vector <int> a;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] != s2[i]) {
+                a.push_back(i);
+            }
+        }
+        
+        int n = a.size();
+        if(n&1){
+            return -1;
+        }
+        vector<int> dp(n + 1, 10000000);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++){
+            dp[i] = dp[i - 1] + x;
+            if (i >= 2) {
+                dp[i] = min(dp[i], dp[i - 2] + 2 * (a[i - 1] - a[i - 2]));
+            }
+        }
+        
+        return dp[n] / 2;
+    }
+};
