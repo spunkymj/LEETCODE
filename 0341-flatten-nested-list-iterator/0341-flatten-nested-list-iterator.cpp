6d747181da7e75112ea3ed1a4c299d4bb45a1936
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-10-20 10:46:33 +0530

    Time: 8 ms (43.18%), Space: 14.8 MB (33.14%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
@@ -0,0 +1,65 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+public:
+    vector<int> v;
+    int ptr;
+
+    void Nested(NestedInteger &temp){
+        vector<NestedInteger> nestedList=temp.getList();
+        for(int i=0 ; i<nestedList.size() ; i++){
+            if(nestedList[i].isInteger()){
+                cout<<nestedList[i].getInteger()<<" ";
+                v.push_back(nestedList[i].getInteger());
+            }
+            else{
+                Nested(nestedList[i]);
+            }
+        }
+        return;
+    }
+
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for(int i=0 ; i<nestedList.size() ; i++){
+            if(nestedList[i].isInteger()){
+                cout<<nestedList[i].getInteger()<<" ";
+                v.push_back(nestedList[i].getInteger());
+            }
+            else{
+                Nested(nestedList[i]);
+            }
+        }
+        ptr=-1;
+    }
+    
+    int next() {
+        ptr++;
+        return v[ptr];
+    }
+    
+    bool hasNext() {
+        return ptr!=v.size()-1;
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
