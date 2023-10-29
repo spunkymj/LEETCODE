1497d7fbd4ceb583097bdad7e3aa7726c84351b1
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-10-29 21:49:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.93%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int attempts=(minutesToTest+minutesToDie-1)/minutesToDie;
+        int attempts=(minutesToTest)/minutesToDie;
         int pigs=0;
         while(pow(attempts+1,pigs)<buckets){
             pigs++;
