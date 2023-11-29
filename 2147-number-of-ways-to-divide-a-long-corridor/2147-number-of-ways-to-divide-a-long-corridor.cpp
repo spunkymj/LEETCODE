0a5c79bd4450959a469bf9da11bdb9f153be3732
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-11-29 20:49:35 +0530

    Time: 137 ms (53.29%), Space: 79.9 MB (21.56%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,60 +1,21 @@
 class Solution {
 public:
-    // Store 1000000007 in a variable for convenience
-    const int MOD = 1e9 + 7;
-    
-    // Count the number of ways to divide from "index" to the last index
-    // with "seats" number of "S" in the current section
-    int count(int index, int seats, string& corridor, int cache[][3]) {
-        // If we have reached the end of the corridor, then
-        // the current section is valid only if "seats" is 2
-        if (index == corridor.length()) {
-            return seats == 2 ? 1 : 0;
-        }
-
-        // If we have already computed the result of this sub-problem,
-        // then return the cached result
-        if (cache[index][seats] != -1) {
-            return cache[index][seats];
-        }
-
-        // Result of the sub-problem
-        int result = 0;
-
-        // If the current section has exactly 2 "S"
-        if (seats == 2) {
-            // If the current element is "S", then we have to close the
-            // section and start a new section from this index. Next index
-            // will have one "S" in the current section
-            if (corridor[index] == 'S') {
-                result = count(index + 1, 1, corridor, cache);
-            } else {
-                // If the current element is "P", then we have two options
-                // 1. Close the section and start a new section from this index
-                // 2. Keep growing the section
-                result = (count(index + 1, 0, corridor, cache) + count(index + 1, 2, corridor, cache)) % MOD;  
-            }
-        } else {
-            // Keep growing the section. Increment "seats" if present
-            // element is "S"
-            if (corridor[index] == 'S') {
-                result = count(index + 1, seats + 1, corridor, cache);
-            } else {
-                result = count(index + 1, seats, corridor, cache);
+    const int mod=1e9+7;
+    int numberOfWays(string c) {
+        vector<long long> pos(0);
+        for(int i=0 ; i<c.size() ; i++){
+            if(c[i]=='S'){
+                pos.push_back(i);
             }
         }
-
-        // Memoize the result, and return it
-        cache[index][seats] = result;
-        return cache[index][seats];
-    }
-
-    int numberOfWays(string corridor) {
-        // Cache the result of each sub-problem
-        int cache[corridor.length()][3];
-        memset(cache, -1, sizeof(cache));
-
-        // Call the count function
-        return count(0, 0, corridor, cache);
+        if(pos.size()%2==1 || pos.size()<2){
+            return 0;
+        }
+        long long ans=1;
+        for(int i=1 ; i<(pos.size()-1) ; i+=2){
+            ans*=(pos[i+1]-pos[i]);
+            ans%=mod;
+        }
+        return ans;
     }
 };
