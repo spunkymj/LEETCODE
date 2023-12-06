5636b35b3835a9e3afa69ec2f8bbe3095b67a471
Manan Jain <101415751+spunkymj@users.noreply.github.com>
2023-12-06 10:01:26 +0530

    Time: 3 ms (13.68%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int totalMoney(int n) {
+        int gp_len=n/7;
         int ans=0;
-        int curr=0;
-        for(int i=0 ; i<n ; i++){
-            if(i%7==0){
-                curr++;
-                ans+=curr;
+        for(int i=1 ; i<=7 ; i++){
+            if(i<=(n%7)){
+                ans+=(((gp_len+1))*(2*i+(gp_len)))/2;
             }
             else{
-                ans+=curr+(i%7);
+                ans+=(((gp_len))*(2*i+(gp_len-1)))/2;
             }
         }
         return ans;
